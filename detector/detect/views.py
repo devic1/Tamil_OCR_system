25e8b559b61c0600601fa9f76d568824dcccaef5
Devic <deepakvictor7585@gmail.com>
2023-03-06 17:09:02 +0530

    added models

--- detector/detect/views.py
+++ detector/detect/views.py
@@ -10,6 +10,37 @@ import os
 import time
 from .models import TextPrediction
 from django.http import JsonResponse
+import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import re
+from PIL import Image, ImageDraw
+from torchvision import transforms
+import json
+import torch.nn as nn
+from torch import optim
+import gc
+
+"""
+device = torch.device("cpu")
+#device = (torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
+print(device)
+model1 = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
+num_classes = 2
+in_features = model1.roi_heads.box_predictor.cls_score.in_features
+model1.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+model2 = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=None)
+model3 = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=None)
+model1.load_state_dict(torch.load("./linemodelv2.pth"))
+in_features = model2.roi_heads.box_predictor.cls_score.in_features
+model2.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+model2.load_state_dict(torch.load("./worddetectionmodelv2.pth"))
+model2.eval()
+in_features = model3.roi_heads.box_predictor.cls_score.in_features
+model3.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+model3.load_state_dict(torch.load("./letterdetectionmodelv2.pth"))
+model3.eval()
+model1.eval()"""
 
 def index(request):
     context = {}
@@ -17,14 +48,52 @@ def index(request):
 
 @api_view(['POST'])
 def uploaded(request):
+    device = torch.device("cpu")
+#device = (torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
+    print(device)
+    model1 = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
+    num_classes = 2
+    in_features = model1.roi_heads.box_predictor.cls_score.in_features
+    model1.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model2 = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=None)
+    model3 = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=None)
+    model1.load_state_dict(torch.load("./linemodelv2.pth"))
+    in_features = model2.roi_heads.box_predictor.cls_score.in_features
+    model2.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model2.load_state_dict(torch.load("./worddetectionmodelv2.pth"))
+    model2.eval()
+    in_features = model3.roi_heads.box_predictor.cls_score.in_features
+    model3.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model3.load_state_dict(torch.load("./letterdetectionmodelv2.pth"))
+    model3.eval()
+    model1.eval()
+    st = time.time()
     dt = request.data
     img = dt["image_data"]
     image_data = img.split(',')[1]
     image_bytes = base64.b64decode(image_data)
     image = Image.open(BytesIO(image_bytes))
-
-    cwd = os.getcwd()
-    text = "Right Time --> "+str(time.time())
+    img = image.convert("RGB")
+    img = transforms.ToTensor()(img)
+    img = img.unsqueeze(0)
+    testout = model1(img)
+    j = 0
+    for i in range(len(testout[0]["boxes"])):
+        if testout[0]["scores"][i] > 0.8:
+            li = testout[0]["boxes"][i].cpu().detach().numpy()
+            j += 1
+            cropimg = transforms.functional.crop(img,int(li[1]),int(li[0]),int(li[3]-li[1]),int(li[2]-li[0]))
+            gc.collect()
+            senout = model2(cropimg)
+            for k in senout:
+                for c,f in zip(k["boxes"],k["scores"]):
+                    print("ok")
+                    if f.item() > 0.8:
+                        wordimg = transforms.functional.crop(cropimg,int(c[1]),int(c[0]),int(c[3]-c[1]),int(c[2]-c[0]))
+                        letout = model3(wordimg)
+                        print(letout)
+    et = time.time()
+    text = "Right Time --> "+str(et-st)
     k = TextPrediction.objects.filter(id=1)
     k.update(textdata=text)
     return Response("Nice")
